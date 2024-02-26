e0e989c3c893b52f3c49f70f6e0490f4971c959c
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-26 22:39:51 +0530

    Created urls

--- todoprojekt/todo/views.py
+++ todoprojekt/todo/views.py
@@ -12,9 +12,41 @@ class TaskList(APIView):
         return Response(serializer.data)
     
     def post(self, request):
-        serializer = TaskSerializer(tasks,many=True)
+        serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return None
+        
+    def get(self,request,pk):
+        task = self.get_object(pk)
+        if not task:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        task = self.get_object(pk)
+        if not task:
+            return Response(status=status.HTTP_404_NOT_FOUND)        
+        serializer = TaskSerializer(instance=task,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        task = self.get_object(pk)
+        if not task:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
     
